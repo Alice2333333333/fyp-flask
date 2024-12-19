6bed6816794d703b8c21f0f8b96aa5340c67a1e2
Alice2333333333 <alicelau0080@gmail.com>
2024-12-19 18:51:28 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,74 @@
+from flask import Flask, jsonify, request
+import firebase_admin
+from firebase_admin import credentials, firestore
+import json
+import os
+
+app = Flask(__name__)
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+# Path to the JSON file in WSL
+json_file_path = r"\\wsl$\Ubuntu\home\alice\fyp\data\monitor_usage.json"
+
+# Get the data from ubuntu
+@app.route("/monitor-usage", methods=["GET"])
+def get_monitor_usage_data():
+    if os.path.exists(json_file_path):
+        try:
+            with open(json_file_path, "r") as json_file:
+                data = json.load(json_file)
+            return data
+        except Exception as e:
+            return jsonify({"error": str(e)}), 500
+    else:
+        return jsonify({"error": "File not found"}), 404
+
+# Receive asset id from flutter and insert data into firebase
+@app.route('/send-assetid', methods=['POST'])
+def receive_asset_id():
+    try:
+        request_data = request.get_json()
+        assetid = request_data.get('assetid')
+
+        if not assetid:
+            return jsonify({"error": "No Asset ID provided"}), 400
+
+        asset_doc_ref = db.collection("asset").document(assetid)
+        asset_doc = asset_doc_ref.get()
+
+        if not asset_doc.exists:
+            return jsonify({"error": f"Asset ID {assetid} not found in Firestore"}), 404
+        
+        monitor_data = get_monitor_usage_data()
+        if isinstance(monitor_data, tuple):
+            return monitor_data
+
+        subcollection_ref = asset_doc_ref.collection("data")
+        for item in monitor_data:
+            status = item['status']
+            count = item['count']
+
+            subcollection_ref.document(status).set({
+                "count": count,
+            }, merge=True)
+
+        print(f"Monitor usage data inserted into subcollection for Asset ID {assetid}")
+        return jsonify({"message": f"Data for Asset ID {assetid} updated successfully"}), 200
+    
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({"error": str(e)}), 500
+    # data = request.get_json()
+    # assetid = data.get('assetid')
+
+    # if assetid:
+    #     print(f"Received Asset ID: {assetid}")
+    #     return jsonify({"message": "Asset ID received successfully"}), 200
+    # else:
+    #     return jsonify({"error": "No Asset ID provided"}), 400
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
